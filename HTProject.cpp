0e900ff957ad3b80773a19a2e529f66e81ab78e5
Beamdamon <Beamdamon@gmail.com>
2023-02-04 12:33:21 -0700

    Added retrieve function and main program

--- HTProject.cpp
+++ HTProject.cpp
@@ -11,10 +11,11 @@ public:
     HT(int V); //Constructor
     void insertItem(int x);
     void deleteItem(int key);
+    void retrieveItem(int key);
     void display();
     int hash(int x)
     {
-        return (x % 598); // Prime Number 
+        return (x % 101); // Prime Number Modulo
     }
 };
 
@@ -51,6 +52,30 @@ void HT::deleteItem(int key)
         hashTable[index].erase(i);
 }
 
+void HT::retrieveItem(int key)
+{
+    // Gets the index of the key
+    int index = hash(key);
+
+    // Finds the key in the list going from beginning to end of the list
+    list <int> ::iterator i;
+    for (i = hashTable[index].begin();
+        i != hashTable[index].end(); i++)
+    {
+        if (*i == key)
+            break;
+    }
+
+    // Deletes the index
+    if (i != hashTable[index].end())
+        cout << key << " Is in the table at index: " << index;
+
+    if (i == hashTable[index].end())
+    {
+        cout << key << " Is not in the table. " << index;
+    }
+}
+
 void HT::display()
 {
     // Loops through to the end of the list and displays the index and key
@@ -66,5 +91,64 @@ void HT::display()
 
 int main()
 {
+    //Main Program
 
+    int key = 0;
+
+    //Creates a HT with a value of 100
+    HT h(100);
+
+    //Adds 1-100 to the hash table
+    for (int i = 0; i < 100; i++)
+    {
+        h.insertItem(rand());
+    }
+
+    h.display();
+
+    while (key != -1)
+    {
+        cout << endl << "Menu" << endl;
+        cout << "1 = Display the Hash Table" << endl;
+        cout << "2 = Insert a value" << endl;
+        cout << "3 = Delete a value" << endl;
+        cout << "4 = Retrieve a value" << endl;
+        cout << "-1 = Exit" << endl;
+        cin >> key;
+
+        switch (key)
+        {
+        case 1:
+        {
+            cout << endl;
+            h.display();
+            break;
+        }
+        case 2:
+        {
+            cout << endl << "Input the number that you want to add.\n";
+            cin >> key;
+            h.insertItem(key);
+            cout << endl << key << " Was added.\n";
+            break;
+        }
+        case 3:
+        {
+            cout << endl << "Input the number that you want to delete.\n";
+            cin >> key;
+            h.deleteItem(key);
+            cout << endl << key << " was deleted.";
+            break;
+        }
+        case 4:
+        {
+            cout << endl << "Input the number that you want to retrieve.\n";
+            cin >> key;
+            h.retrieveItem(key);
+            break;
+        }
+        }
+    }
+
+    return 0;
 }
